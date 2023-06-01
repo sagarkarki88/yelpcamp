48eaebbbc2eab6a96968535893df12b2b968e9f8
sagar <sagarkarki88@gmail.com>
2023-06-01 11:00:19 +1000

    adding httpOnly option to restrict access to cookes form javascript

--- index.js
+++ index.js
@@ -75,7 +75,9 @@ const sessionConfig  = {
     resave : false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 1 *60*60*24*7
+        httpOnly:true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge:1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig));
